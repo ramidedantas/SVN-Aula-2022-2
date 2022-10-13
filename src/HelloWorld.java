e0275f23f0fed70fddba093da9b2beb12831bdbb
PeterNV <114534711+PeterNV@users.noreply.github.com>
2022-10-13 18:22:18 -0300

    COMMIT REALIZADO POR PEDRO VILA NOVA

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-        System.out.println("Modificação Eclipse+CMD");
+        System.out.println("ModificaÃ§Ã£o Eclipse+CMD");
+        System.out.println("Pedro Vila Nova");
 	}
 
 }
